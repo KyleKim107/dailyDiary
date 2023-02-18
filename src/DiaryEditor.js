a9ab8ebb5dc472b789de18fadfbafe841a387ed0
Kyle Kim <dkim577@wisc.edu>
2023-02-18 21:12:43 +0900

    Event callback

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -6,12 +6,19 @@ const DiaryEditor = () =>{
     <div className="DiaryEditor">
         <h2>오늘의 일기</h2>
         <div>
-            <input value={author} onChange={(e) => { //값이 바뀌었을때 
-                console.log(e)
-            }}/>
+            <input 
+            name = "author"
+            value={author}
+            onChange={(e) => { //값이 바뀌었을때 전달되는 event객체
+                console.log(e.target.value) // evetn객체의 target의 value.
+                console.log(e.target.name) // evetn객체의 target의 name
+                setAuthor(e.target.value)
+            }}
+            />
         </div>
     </div>
     );
 }
+
 
 export default DiaryEditor;
