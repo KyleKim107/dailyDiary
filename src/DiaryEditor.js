1f2ec001d450eab0f27d0f1a1d5f998ae7b5010b
Kyle Kim <dkim577@wisc.edu>
2023-02-18 21:42:01 +0900

    Binding States to the one

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,19 +1,37 @@
 import { useState } from "react";
 
 const DiaryEditor = () =>{
-    const [author, setAuthor] = useState("김대호");
+    const [state, setState] = useState({ // setState들을 하나로 묶는다
+        author:"",
+        content:"",
+    });
+    // const [author, setAuthor] = useState("");
+    // const [content , setContent] = useState("")
     return (
     <div className="DiaryEditor">
         <h2>오늘의 일기</h2>
         <div>
             <input 
             name = "author"
-            value={author}
+            value={state.author}
             onChange={(e) => { //값이 바뀌었을때 전달되는 event객체
-                console.log(e.target.value) // evetn객체의 target의 value.
-                console.log(e.target.name) // evetn객체의 target의 name
-                setAuthor(e.target.value)
+                setState({
+                    author: e.target.value,
+                    content: state.content
+                })
             }}
+            />
+        </div>
+        <div>
+            <textarea 
+                value={state.content} 
+                onChange={(e) => { //값이 바뀌었을때 전달되는 event객체
+                    setState({
+                        author: state.content ,
+                        content: e.target.value,
+                    })
+                }
+            }
             />
         </div>
     </div>
