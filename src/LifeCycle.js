c57982676696e0abf3cb16688806ffae521ac5ed
Kyle Kim <dkim577@wisc.edu>
2023-02-20 22:54:37 +0900

    Control Mount Unmount

--- src/LifeCycle.js
+++ src/LifeCycle.js
@@ -1,38 +1,37 @@
 import React, {useEffect , useState} from "react";
 
-const LifeCycle = () => {
+const UnMountTest = () => {
+    useEffect( () =>{
+        console.log("Mount!") // Mount 시점에 실행되게 한다.
 
-    const [count,setCount] = useState(0);
-    const [text,setText]= useState("");
 
-    useEffect(() =>{ // 컴포넌트가 마운트 될따 작업을 하고 싶으면 두번째 인자에 빈 배열을 리턴하고 콜백함수에 뭔가를 넣어라.
-        console.log("Mount!")
+        return () => { // 함수를 하나 리턴하게 하면 unMount시점에 실행되게 된다.
+            // UnMount 시점에 실행되게 한다.
+            console.log("UnMount!!")
+        };
     }, []);
 
-    useEffect(() => { // Component가 업데이트 되는 순간에 어떤 작업을 수행하고 싶다면 callback함수에 작업을 넣어라.
-        console.log("Update")
-    })
+    return <div>UnMount Testing Component</div>
+}
 
-    useEffect(() =>{
-        console.log(`count is update : ${count}`)
-        if(count > 5){
-            alert("count가 5를 넘었습니다. 따라서 1로 초기화합니다!");
-            setCount(1);
-        }
-    } ,[count] )// 두번째 인자에 있는 배열에 인지하고 싶은 인자만 배열에 넣어버 리턴하자.
+const LifeCycle = () => {
 
-    useEffect(() =>{
-        console.log(`text is update : ${text}`)
-    } ,[text] )
+    const [isVisible,setIsVisible] = useState(false);
+    const toggle = () => setIsVisible(!isVisible);
+
+    return(
+        <div style={{padding:20}}>
+            <button onClick={toggle}>ON/OFF</button>
+            {isVisible && <UnMountTest/>} 
+        </div>
+    );
+
+    
+
+
 
     return <div style={{padding:20}}>
-        <div>
-        {count}
-        <button onClick={()=> setCount(count+1)}> + </button>
-        </div>
-        <div>
-            <input value={text} onChange={(e) => setText(e.target.value)}/>
-        </div>
+        
     </div>;
 }
 
