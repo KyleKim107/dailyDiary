f6ac0af8c14eaf932a9ee57b5c92e81732dd6cea
Kyle Kim <dkim577@wisc.edu>
2023-05-03 01:32:06 +0900

    새 페이지에서 emotion 선택하기까지

---
+++ src/components/EmotionItem.js
@@ -0,0 +1,15 @@
+const EmotionItem = ({emotion_id
+    , emotion_img
+    , emotion_descript
+    , onclick
+    ,isSelected
+}) =>{
+    return (<div 
+        onClick={() => onclick(emotion_id)}  
+        className={["EmotionItem", isSelected ? `EmotionItem_on_${emotion_id}` : `EmotionItem_off` ].join(" ")}>
+    <img src={emotion_img} />
+    <span>{emotion_descript}</span>
+    </div>);
+}
+
+export default EmotionItem;
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,33 +1,10 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import MyHeader from "../components/MyHeader";
-import MyButton from "../components/MyButton";
-
-const getStringDate = (date) =>{
-  return date.toISOString().slice(0,10);
-}
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
-  console.log(getStringDate(new Date()))
-const naviagte = useNavigate();
-const [date, setDate] = useState();
   return (
     <div>
-      <MyHeader 
-      headText={"새 일기쓰기"}
-      leftChild={<MyButton text={"< 뒤로가기"} onclick={() => naviagte(-1)} />}
-       />
-       <div>
-          <section>
-            <h4>오늘은 언제인가요?</h4>
-            <div className="input-box">
-              <input 
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                type="date"/>
-            </div>
-          </section>
-       </div>
+      <DiaryEditor/>
     </div>
   );
 };
